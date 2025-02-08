724341860bfce616068316a6159cccb8cd168fcf
thayinm <thayinm@amazon.com>
2025-02-08 12:04:18 +0200

    fixed fsx module

--- Terraform/modules/fsx/main.tf
+++ Terraform/modules/fsx/main.tf
@@ -2,6 +2,7 @@ resource "aws_fsx_lustre_file_system" "fsx_lustre_hyperpod" {
   subnet_ids       = [var.private_subnet]
   storage_capacity = 1200
   storage_type = "SSD"
+  per_unit_storage_throughput = 250
   deployment_type = "PERSISTENT_2"
   log_configuration {
     level = "WARN_ERROR"
